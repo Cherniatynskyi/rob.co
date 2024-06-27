630320cdf0e4669d0b9697a2c6399dfbaedb860f
Mark Cherniatynskyi <markushalot34@gmail.com>
2024-06-27 12:52:24 +0300

    upd

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -5,11 +5,12 @@ export const ProductCard = ({item}) => {
   const score = item.reviews.reduce((avgScore, el) => {
     return avgScore + parseInt(el.score)
   }, 0)
+  const title = item.title.length > 38 ? `${item.title.slice(0, 38)}...` : item.title
   return (
     <li className={css.cardItem}>
         <img className={css.cardImg} src={item.photo_urls[0]} alt="" />
         <div>
-          <h3 className={css.cardTitle}>{item.title}</h3>
+          <h3 className={css.cardTitle}>{title}</h3>
           <Rating rating={score / item.reviews.length}/>
           <p className={css.cardPrice}>₴ {item.price}</p>
         </div>
