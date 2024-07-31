fb3705c98dfd3ada1a6d833942a6c5b0273ea250
Cherniatynskyi <87269655+Cherniatynskyi@users.noreply.github.com>
2024-07-31 09:10:06 +0000

    Deploying to gh-pages from @ Cherniatynskyi/rob.co@a8743a2521ea45932b11ec85030af88babd7ac73 🚀

---
+++ static/js/720.22c8ed8d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrob_co=self.webpackChunkrob_co||[]).push([[720],{6720:(t,h,a)=>{a.d(h,{b6i:()=>l});var c=a(3441);function l(t){return(0,c.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"},child:[]},{tag:"path",attr:{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=720.22c8ed8d.chunk.js.map
